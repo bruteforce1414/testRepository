39d1fae41a0c87056794ce4daccf0c4de191285d
bruteforce1414 <bruteforce1414@gmail.com>
2018-11-23 16:20:50 +0500

    Реализация прерывания выполнения клиента и сервера по нажатию Crtl+C

--- ClientServerTest/Client.go
+++ ClientServerTest/Client.go
@@ -4,19 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 
-	conn, _ := net.Dial("tcp", "127.0.0.1:4547")
-	for {
+	    go func() {
+    	for {
+
+		conn, err:= net.Dial("tcp", "127.0.0.1:4547")
+		    if err != nil {
+			fmt.Println(err)
+			break;
+			//return
+			}
 				message, _ := bufio.NewReader(conn).ReadString('\n')
 				fmt.Print("Message from server: "+message)
+				time.Sleep(1 * time.Second)}
+	}()
 
+	sigsClient := make(chan os.Signal, 1)
+	doneClient := make(chan bool, 1)
+	signal.Notify(sigsClient, syscall.SIGINT, syscall.SIGTERM)
+	handleClient(sigsClient, doneClient)
 
-	}
-
- fmt.Scanln()
 }
 
+func handleClient(sigs chan os.Signal,done chan bool) {
+	s := <-sigs // ожидание сигнала
+	fmt.Println("Программа завершится через 3 секунды. Возникшее событие:", s)
+	time.Sleep(3 * time.Second)
+	done <- true
+	os.Exit(0)
+
+}
 
--- ClientServerTest/Server.go
+++ ClientServerTest/Server.go
@@ -4,32 +4,59 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 var letters =[10]string{"one","two","three","four","five","six","seven","eight","nine","ten"}
 var message string
-func main() {
-	fmt.Println("Launching server...")
 
+func main() {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	sigsServer := make(chan os.Signal, 1)
+	doneServer := make(chan bool, 1)
 	listener, err := net.Listen("tcp", ":4547")
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-    conn, err := listener.Accept()
-
+	fmt.Println("Launching server...")
+go func() {
 	for {
-		rand.Seed(time.Now().UTC().UnixNano())
-		message=randSeq(rand.Intn(10))
-		conn.Write([]byte(message + "\n"))
-		time.Sleep(1 * time.Second)
-	}
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go handleConnection(conn)
 
 	}
+}()
+	signal.Notify(sigsServer, syscall.SIGINT, syscall.SIGTERM)
+	handleServer(sigsServer, doneServer)
+	time.Sleep(100 * time.Second)
+	}
+
+func  handleConnection (conn net.Conn)  {
+	message = randSeq(rand.Intn(10))
+	conn.Write([]byte(message + "\n"))
+	time.Sleep(1 * time.Second)}
+
 
 func randSeq(n int) string {
 	i:=rand.Intn(len(letters))
 	return string(letters[i])
 	}
 
+
+func handleServer(sigs chan os.Signal,done chan bool) {
+	s := <-sigs // ожидание сигнала
+	fmt.Println("Программа завершится через 3 секунды. Возникшее событие:", s)
+	time.Sleep(3 * time.Second)
+	done <- true
+	os.Exit(0)
+}
